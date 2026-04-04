5246c32743ecec9d2e7db8f940bd386ee7b8bee5
tommy <doviethung.5409@gmail.com>
2026-04-04 15:43:11 +0700

    fix bug load ml_model

--- pipeline/app_gradio_hybrid.py
+++ pipeline/app_gradio_hybrid.py
@@ -189,7 +189,7 @@ def build_orchestrator(args) -> DialogueOrchestrator:
     rule_policy = RuleBasedPolicy()
     # ml_policy = SklearnMLPolicy(model_path=args.ml_model, debug=args.debug) if args.ml_model else None
     # llm_policy = HuggingFaceLLMPolicy(model_name=args.llm_model, debug=args.debug) if args.llm_model else None
-    ml_policy = SklearnMLPolicy(args.ml_policy_path) if args.ml_policy_path else None
+    ml_policy = SklearnMLPolicy(args.ml_model) if args.ml_model else None
 
     llm_policy = None
    
